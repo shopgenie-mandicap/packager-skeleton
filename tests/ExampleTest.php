3498e0fe3d5051b2495e8d62aa19f6411a258e3d
shopgenie-mandicap <amanda.romero@shopgenie.io>
2024-10-11 13:12:41 -0600

    Updates skeleton to use Pest

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,13 +1,4 @@
 <?php
-
-namespace :uc:vendor\:uc:package\Tests;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    public function test_true_is_true(): void
-    {
-        self::assertTrue(true);
-    }
-}
+test('example', function () {
+    expect(true)->toBeTrue();
+});
